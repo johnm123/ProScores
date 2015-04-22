f96d98c7990e9b2fbec2690d0332b0a8fb3097be
johnm123 <johnmoss@clara.co.uk>
2015-04-22 21:49:15 +0100

    Add view model and Db stuff

--- ProScores/Controllers/HomeController.cs
+++ ProScores/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ProScores.Models;
+using ProScores.Objects;
 
 namespace ProScores.Controllers
 {
@@ -10,7 +12,37 @@ namespace ProScores.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var results = new List<ProEvoResult>()
+            {
+                new ProEvoResult()
+                {
+                    PlayerHome = "John",
+                    PlayerAway = "Phillip",
+                    GoalsHome = 5,
+                    GoalsAway = 1,
+                    Date = DateTime.Now
+                },
+                new ProEvoResult()
+                {
+                    PlayerHome = "Jacob",
+                    PlayerAway = "Peter",
+                    GoalsHome = 3,
+                    GoalsAway = 5,
+                    Date = DateTime.Now
+                },
+                new ProEvoResult()
+                {
+                    PlayerHome = "Sam",
+                    PlayerAway = "Jason",
+                    GoalsHome = 2,
+                    GoalsAway = 0,
+                    Date = DateTime.Now
+                }
+            };
+            
+            var vm = new ScoresListPageViewModel() { Results = results };
+
+            return View(vm);
         }
     }
 }
---
+++ ProScores/Models/ScoresListPageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using ProScores.Objects;
+
+namespace ProScores.Models
+{
+    public class ScoresListPageViewModel
+    {
+        public IEnumerable<ProEvoResult> Results { get; set; }
+    }
+}
