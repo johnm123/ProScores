cc7a0adc7fca5637465273203ba1ee192258e4af
johnm123 <johnm00001@gmail.com>
2015-06-22 15:31:58 +0100

    Add stuff to fake repo.

--- ProScores.Data/FakeProScoresDataStore.cs
+++ ProScores.Data/FakeProScoresDataStore.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 
@@ -9,32 +10,64 @@ namespace ProScores.Data
     {
         public IEnumerable<ProEvoResult> GetAll()
         {
-            return new List<ProEvoResult>();
+            return GetFakeResults();
         }
 
         public IEnumerable<Player> GetAllPlayers()
         {
             var players = new List<Player>
             {
-                new Player() { NickName = "Rimmer", Name = "Shaun" },
-                new Player() { NickName = null, Name = "Jabroni" },
-                new Player() { NickName = "The King", Name = "John.M" }
+                new Player() {NickName = "Rimmer", Name = "Shaun"},
+                new Player() {NickName = null, Name = "Jabroni"},
+                new Player() {NickName = "The King", Name = "John.M"}
             };
             return players;
         }
 
         public ProEvoResult CreateOrModify(ProEvoResult result)
         {
-            return new ProEvoResult();
+            result.Id = 555;
+            return result;
         }
 
         public Player Create(Player player)
         {
-            return new Player();
+            return player;
         }
 
         public void Delete(int id)
         {
+        }
+
+        private static List<ProEvoResult> GetFakeResults()
+        {
+            return new List<ProEvoResult>()
+            {
+                new ProEvoResult()
+                {
+                    Id = 1,
+                    PlayerHome = "Shaun",
+                    TeamHome = "Chelsea",
+                    GoalsHome = 0,
+                    PlayerAway = "John.M",
+                    TeamAway = "Real Madrid",
+                    GoalsAway = 5,
+                    Comments = "Easy",
+                    Date = DateTime.Now
+                },
+                new ProEvoResult()
+                {
+                    Id = 2,
+                    PlayerHome = "Shaun",
+                    TeamHome = "Chelsea",
+                    GoalsHome = 1,
+                    PlayerAway = "John.M",
+                    TeamAway = "Real Madrid",
+                    GoalsAway = 3,
+                    Comments = "Easy still",
+                    Date = DateTime.Now
+                },
+            };
         }
     }
 }
